d28b1e7dbc104d46df0ff64a66f228217e24419d
Tyler Harbin-Giuntoli <harbin.giuntoli@gmail.com>
2026-09-09 17:26:31 -0700

    Give public package an independent Lake target

--- lakefile.lean
+++ lakefile.lean
@@ -2,11 +2,11 @@ import Lake
 
 open Lake DSL
 
-package DomainScaling
+package TheoremLibrary
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @
   "58e016c6f6c829b5f25b1a87a88f495f40e70aa7"
 
 @[default_target]
-lean_lib DomainScaling
+lean_lib TheoremLibrary
